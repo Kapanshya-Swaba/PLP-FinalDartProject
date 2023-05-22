8380f5ad368f41d38a5bd4f4f0d305de4bf6d840
lovely-mine <82084573+lovely-mine@users.noreply.github.com>
2023-05-22 21:58:51 +0200

    Add files via upload

--- lib/Components/Pages/Cart_Products.dart
+++ lib/Components/Pages/Cart_Products.dart
@@ -8,62 +8,55 @@ class Cart_products extends StatefulWidget {
 }
 
 class _Cart_productsState extends State<Cart_products> {
-
   @override
   Widget build(BuildContext context) {
-    var Products_on_the_cart =[
+    var Products_on_the_cart = [
       {
-        "name":"Hard Drive 1TB",
-        "picture":"assets/Products/HardDrive.jpg",
-
-        "price":"950",
+        "name": "Hard Drive 1TB",
+        "picture": "assets/Products/HardDrive.jpg",
+        "price": "950",
         "Size": "1TB",
-        "Quantity":1,
-      },
-
-      {
-        "name":"Mac-Book",
-        "picture":"assets/Products/Mac.jpg",
-        "price":"10000",
-        "Size": "256GB",
-        "color": "White",
-        "Quantity":1,
-
-
-      },
-
-      {
-        "name":"Phone Stand",
-        "picture":"assets/Products/img(21).jpg",
-
-        "price":"45",
-        "color": "White",
-        "Quantity":1,
-      },
-      {
-        "name":"Patch Panel",
-        "picture":"assets/Products/img(24).jpg",
-        "price":"k2500",
-        "color":"white",
+        "color":"No color",
         "Quantity": 1,
       },
-
+      {
+        "name": "Mac-Book",
+        "picture": "assets/Products/Mac.jpg",
+        "price": "10000",
+        "Size": "256GB",
+        "color": "White",
+        "Quantity": 1,
+      },
+      {
+        "name": "Phone Stand",
+        "picture": "assets/Products/Stand.jpg",
+        "price": "45",
+        "color": "White",
+        "Quantity": 1,
+      },
+      {
+        "name": "Patch Panel",
+        "picture": "assets/Products/Patch Panel.jpg",
+        "price": "2500",
+        "color": "white",
+        "Quantity": 1,
+      },
     ];
     return ListView.builder(
-      itemCount: Products_on_the_cart.length,
-      itemBuilder:  (context, index){
-        return Single_cart_product(
-          cart_product_name: Products_on_the_cart[index]["name"],
-          cart_product_Size: Products_on_the_cart[index]["color"],
-          cart_product_Quantity: Products_on_the_cart[index]["Quantity"],
-          cart_product_price: Products_on_the_cart[index]["price"],
-          cart_product_picture: Products_on_the_cart[index]["picture"],
-
-
-        );
-      });
+        itemCount: Products_on_the_cart.length,
+        itemBuilder: (context, index) {
+          return Single_cart_product(
+            cart_product_name: Products_on_the_cart[index]["name"],
+            cart_product_Size: Products_on_the_cart[index]["Size"]??"No Size",
+            cart_product_color: Products_on_the_cart[index]["color"]??"No Color",
+            cart_product_Quantity: Products_on_the_cart[index]["Quantity"],
+            cart_product_price: Products_on_the_cart[index]["price"],
+            cart_product_picture: Products_on_the_cart[index]["picture"],
+          );
+        });
   }
 }
+
 class Single_cart_product extends StatelessWidget {
   final cart_product_name;
   final cart_product_picture;
@@ -72,27 +65,27 @@ class Single_cart_product extends StatelessWidget {
   final cart_product_Quantity;
   final cart_product_color;
 
-  const Single_cart_product(
-      {Key? key,
-        this.cart_product_name,
-        this.cart_product_picture,
-        this.cart_product_price,
-        this.cart_product_Quantity,
-        this.cart_product_Size,
-        this.cart_product_color,})
-      : super(key: key
-  );
+  const Single_cart_product({
+    Key? key,
+    this.cart_product_name,
+    this.cart_product_picture,
+    this.cart_product_price,
+    this.cart_product_Quantity,
+    this.cart_product_Size,
+    this.cart_product_color,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Card(
+    return Card(
       child: ListTile(
         // ======Leading Section ========
-         leading: Image.asset(cart_product_picture,
+        leading: Image.asset(
+          cart_product_picture,
           width: 100.0,
           height: 100.0,
         ),
-        // ==== Title Section ====
+        // ======= Title Section ======
         title: Text(cart_product_name),
 
         // === Subtitle ==========
@@ -100,46 +93,60 @@ class Single_cart_product extends StatelessWidget {
           children: [
             //======= Size of product =======
             Row(
-              children: <Widget> [
-             const Padding(
-               padding: EdgeInsets.all(2.0),
-               child:Text("Size: "),
-             ),
+              children: <Widget>[
+                const Padding(
+                  padding: EdgeInsets.all(4.0),
+                  child: Text("Size: "),
+                ),
                 Padding(
                   padding: const EdgeInsets.all(4.0),
-                  child:Text(cart_product_Size, style: const TextStyle(color:Colors.red),),
+                  child: Text(
+                    cart_product_Size,
+                    style: const TextStyle(color: Colors.red),
+                  ),
                 ),
                 // ======== Product Color =======
-                const Padding(padding: EdgeInsets.fromLTRB(20.0, 8.0, 8.0, 8.0),
-                child: Text("Color"),
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(20.0, 8.0, 8.0, 8.0),
+                  child: Text("Color:"),
                 ),
-                Padding(padding: EdgeInsets.all(4.0),
-                child: Text(cart_product_color,style: const TextStyle(color:Colors.red ),),),
-
-
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Text(
+                    cart_product_color,
+                  ),
+                ),
               ],
             ),
-            // ===== Product Price ===
+            // ========== Product Price ============
             Container(
               alignment: Alignment.topLeft,
-              child:  Text("\$${cart_product_price}", style: TextStyle(fontSize: 16.0,fontWeight: FontWeight.bold,
-              color:Colors.red),
+              child: Text(
+                "\$$cart_product_price",
+                style: const TextStyle(
+                    fontSize: 17.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red),
               ),
             ),
           ],
         ),
+
         trailing: Column(
           children: <Widget>[
-            IconButton(icon: Icon(Icons.arrow_drop_up),
-              onPressed: () {  },),
+            IconButton(
+              icon: const Icon(Icons.arrow_drop_up),
+              onPressed: () {},
+            ),
             Text("$cart_product_Quantity"),
-            IconButton(icon: Icon(Icons.arrow_drop_down),
-              onPressed: () {  },),
+            IconButton(
+              icon: const Icon(Icons.arrow_drop_down),
+              onPressed: () {},
 
+            ),
           ],
         ),
       ),
     );
   }
 }
-
